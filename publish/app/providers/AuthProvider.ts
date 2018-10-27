042b72c2cdc38756a4e8fb8d46ebd308b98ab26b
Luke Policinski <luke@lukepolo.com>
2018-10-27 14:20:57 -0400

    Forgot to add auth service in the provider

--- publish/app/providers/AuthProvider.ts
+++ publish/app/providers/AuthProvider.ts
@@ -1,8 +1,10 @@
+import AuthService from "@app/services/AuthService";
 import ServiceProvider from "varie/lib/support/ServiceProvider";
 import JwtGuard from "varie-authentication-plugin/lib/guards/jwt/JwtGuard";
 
 export default class AuthenticationServiceProvider extends ServiceProvider {
   public register() {
     this.app.singleton("JwtGuard", JwtGuard);
+    this.app.bind("AuthService", AuthService);
   }
 }

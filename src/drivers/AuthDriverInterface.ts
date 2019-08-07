7c910dc314d10e7712d09585fbc2ec2d13ee7fbc
Luke Policinski <luke.policinski@egov.com>
2019-08-06 21:01:51 -0400

    WIP

--- src/drivers/AuthDriverInterface.ts
+++ src/drivers/AuthDriverInterface.ts
@@ -5,8 +5,6 @@ export default interface AuthDriverInterface {
   loginResponse(response: HttpResponseInterface): Promise<any>;
   logoutResponse(response: HttpResponseInterface): Promise<any>;
   isLoggedIn(guard: string): Promise<boolean>;
-  refreshData?(guard: string): object;
-  refreshResponse?(response: HttpResponseInterface): Promise<any>;
   registerResponse(response: HttpResponseInterface): Promise<any>;
   resetPasswordResponse(response: HttpResponseInterface): Promise<any>;
   middlewareRequest(
